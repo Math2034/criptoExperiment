260c33e301094a0c9eacb73ebffd977ca10e3990
Matheus R <61693870+Math2034@users.noreply.github.com>
2022-09-13 19:21:53 -0300

    Update crypto_comentado.js

--- crypto_comentado.js
+++ crypto_comentado.js
@@ -1,73 +1,22 @@
-function crypt(msg, hash) {
-    // Pega a data em formato de números
-    const d = Date.now();
-    
-    // Transformando a string em um array de letras
-    let phrase = msg.split("");
+const cripto=(str) =>
+{
+    var letras= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let resultado ='';
+    while(str.length>0)
+  {
 
-    // Loop para passar em cada um dos itens do array
-    for (let i = 0; i < phrase.length; i++) {
-        // Transformando cada letra em um decimal de ASCII
-        // e multiplicando pela data
-        phrase[i] = (phrase[i].charCodeAt(0) * d);
+  for (let i=0;i<=25;i++)
+     { 
+       if ((letras[i]===str[0]) && (i>13)) {resultado = resultado + letras[i-13];};
+       if ((letras[i]===str[0]) && (i<13)) {resultado = resultado + letras[i+13] ;} ;
+      };
+ if (!  /^[A-Z]*$/.test(str[0]) ) {resultado = resultado+ str[0];}; 
+str=str.substring(1);
+   };   
+  return resultado;          
+};
 
-        // Adicionando a hash separadora a cada um dos
-        // itens do array (letras)
-        phrase[i] += hash;
-    }
+//digite a mensagem(criptografada ou descriptografada) aqui e terá tanto a mensagem decifrada quanto criptografada
+console.log(cripto('VASCO'));
 
-    // Adicionando a data de criptografia ao ultimo elemento do array
-    phrase.push(d);
-
-    // Variável de resposta
-    let output = "";
-    
-    // Loop adicionando os elementos do array como string
-    for (let i = 0; i < phrase.length; i++) {
-        output += phrase[i];
-        // output = output + phrase[i]
-    }
-
-    // Retorna a mensagem criptografada
-    return output;
-}
-
-function decrypt(cypher, hash) {
-    // Forço a hash a virar uma string
-    let h = String(hash)
-    // Uso a hash para dividir a string em um array
-    const msg = cypher.split(h);
-
-    //Removeu o ultimo item do array e guardou em "d"
-    const d = msg.pop();
-
-    // Passando por cada um dos elementos e:
-    // - dividindo pela data
-    // - transformando de ASCII para caracteres
-    for (let i = 0; i < msg.length; i++) {
-        msg[i] = String.fromCharCode(parseInt(msg[i]) / d);
-    }
-
-    // Defininindo um uma saída no formato de string
-    let output = "";
-
-    // Loop concatenando a minha mensagem
-    for (let i = 0; i < msg.length; i++) {
-        output += msg[i];
-    }
-
-    // Retornando a mensagem descriptografada
-    return output;
-}
-
-const phrase = "O vasco da gama sobe!";
-
-let pass = crypt(phrase, "012345678901234567890123456789");
-
-console.log(pass);
-
-
-
-let solve = decrypt(pass, "012345678901234567890123456789")
-
-console.log(solve);
+//comando para rodar no terminal: node index.js
